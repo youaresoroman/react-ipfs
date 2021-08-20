7556efe383c728d7f17b505dfe5e3f0fa2acb35d
Roman Yankowski <romanjankowski@groupoffice.ch>
2021-08-20 11:01:34 +0200

    ipfs instance constructor update

--- src/useIPFS/index.tsx
+++ src/useIPFS/index.tsx
@@ -1,36 +1,55 @@
 //// eslint-disable
 import Ipfs from "ipfs"
 import { useEffect, useState } from "react"
+import { useStore } from "react-context-hook"
 
 let ipfs: any = null
+
+type ipfsInstance = [
+    {
+        ipfs: any,
+        isIpfsReady: boolean,
+        ipfsInitError: Error | null
+    },
+    (value: unknown) => void,
+    () => void
+]
 
 export default function useIpfsFactory() {
     const [isIpfsReady, setIpfsReady] = useState(Boolean(ipfs))
     const [ipfsInitError, setIpfsInitError] = useState<Error | null>(null)
+    const [instance, setInstance] = useStore('ipfsInstance') as ipfsInstance;
 
     useEffect(() => {
-        startIpfs()
-        return function cleanup() {
-            if (ipfs && ipfs.stop) {
-                console.log("Stopping IPFS")
-                ipfs.stop().catch((err: Error) => setIpfsInitError(err))
-                ipfs = null
-                setIpfsReady(false)
-            }
+        if (instance && instance.isIpfsReady) {
+            ipfs = instance.ipfs
+            setIpfsReady(Boolean(ipfs))
+            setIpfsInitError(instance.ipfsInitError)
+        } else {
+            startIpfs()
         }
-    }, [])
+        // return function cleanup() {
+        //     if (ipfs && ipfs.stop) {
+        //         console.log("Stopping IPFS")
+        //         ipfs.stop().catch((err: Error) => setIpfsInitError(err))
+        //         ipfs = null
+        //         setIpfsReady(false)
+        //     }
+        // }
+    }, [instance])
 
     async function startIpfs() {
-        if (!ipfs) {
-            try {
-                console.log("%cIPFS Started", "color: green")
-                ipfs = await Ipfs.create()
-            } catch (error) {
-                ipfs = null
-                setIpfsInitError(error)
-            }
+        try {
+            console.log("%cIPFS Started", "color: green")
+            await Ipfs.create()
+                .then((ipfsInstance) => {
+                    ipfs == ipfsInstance
+                    setInstance({ ipfs, ipfsInitError, isIpfsReady })
+                })
+        } catch (error) {
+            ipfs = null
+            setIpfsInitError(error)
         }
-
         setIpfsReady(Boolean(ipfs))
     }
 
